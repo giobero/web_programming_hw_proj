c271d732e675a703b221fce08a65dcd4ffcc2ea7
Giorgi Beridze <g.beridze@napr.gov.ge>
2015-03-12 12:21:57 +0400

    reformatt source code

--- js/add_tabs.js
+++ js/add_tabs.js
@@ -1,24 +1,24 @@
-	
-	Ext.onReady(function(){
-		if (Ext.getCmp('tabsId')){
-			Ext.getCmp('tabsId').add({
-				xtype		: 'tabpanel',
-				items		: [{
-									title	: 'tools',
-									height	: 500,
-									items	: [{
-													xtype		: 'tabpanel',
-													tabPosition	: 'bottom',
-													items		: [{
-																		title	: 'Calculator'
-																},{
-																		title	: 'See what time is it',
-																		listeners	: {
-																					activate	: popUpTime
-																		}
-													}]
-									}]
-				}]
-			});
-		}
-	});
+Ext.onReady(function(){
+    if (!Ext.getCmp('tabsId')){
+        return;
+    }
+    Ext.getCmp('tabsId').add({
+        xtype: 'tabpanel',
+        items: [{
+            title: 'tools',
+            height: 500,
+            items: [{
+                xtype: 'tabpanel',
+                tabPosition: 'bottom',
+                items: [{
+                    title: 'Calculator'
+                },{
+                    title: 'See what time is it',
+                    listeners: {
+                        activate: popUpTime
+                    }
+                }]
+            }]
+        }]
+    });
+});
